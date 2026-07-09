0f472895f3753d5a10c8173f16a8d512e80285d4
Gabriel Silveira <gabriel-silveira1@hotmail.com>
2026-07-09 20:16:18 -0300

    SqsController created

---
+++ src/main/java/com/rds/postgre/sqs/controller/SqsController.java
@@ -0,0 +1,41 @@
+package com.rds.postgre.sqs.controller;
+
+import com.rds.postgre.sqs.service.SqsService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/sqs")
+public class SqsController {
+
+    private final SqsService sqsService;
+
+    public SqsController(SqsService sqsService) {
+        this.sqsService = sqsService;
+    }
+
+    @PostMapping("/enviar")
+    public ResponseEntity<String> enviar(@RequestBody String mensagem) {
+        String messageId = sqsService.enviarMensagem(mensagem);
+        return ResponseEntity.ok("Enviado direto pra fila! MessageId: " + messageId);
+    }
+
+    @GetMapping("/visualizar")
+    public ResponseEntity<List<String>> visualizar() {
+        return ResponseEntity.ok(sqsService.visualizarMensagens());
+    }
+
+    @GetMapping("/contagem")
+    public ResponseEntity<Map<String, Integer>> contagem() {
+        return ResponseEntity.ok(Map.of("mensagensNaFila", sqsService.contarMensagens()));
+    }
+
+    @DeleteMapping("/limpar")
+    public ResponseEntity<String> limpar() {
+        sqsService.limparFila();
+        return ResponseEntity.ok("Fila limpa com sucesso");
+    }
+}
